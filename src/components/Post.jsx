9c5db83115ffb7af1eba6e7e57b66030ee92d5d1
Eric Treadway <eric.s.treadway@gmail.com>
2021-09-23 11:22:13 -0500

    made a card with styled components

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,14 @@
+import { Card } from "../style/Card";
+
 export const Post = (props) => {
   console.log(props.value);
   return (
-    <div>
+    <Card>
       <h1>{props.value.title}</h1>
       <h5>
         Post: #{props.value.id + 1} --- Published: {props.value.date}
       </h5>
       <p>{props.value.content}</p>
-    </div>
+    </Card>
   );
 };
---
+++ src/style/Card.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  background-color: none;
+  width: 70%;
+  border: 1px solid grey;
+  border-radius: 6px;
+  padding: 1em;
+  margin: 1em;
+`;

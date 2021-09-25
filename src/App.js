53264f26c508b059d8fdc05f3022c5c3388d4fd5
Eric Treadway <eric.s.treadway@gmail.com>
2021-09-25 18:09:26 -0500

    added basic form for making posts

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Posts } from "./components/Posts";
 import { PostProvider } from "./context/PostsContext";
 import { Wrapper } from "./style/Wrapper";
+import { NewPost } from "./components/NewPost";
 
 function App() {
   return (
     <PostProvider>
       <Wrapper>
         <h1>Hello from my blog!</h1>
+        <NewPost />
         <Posts />
       </Wrapper>
     </PostProvider>

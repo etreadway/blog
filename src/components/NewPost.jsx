021bd35f6b9a066af1d71b2f29907bec394aae3e
Eric Treadway <eric.s.treadway@gmail.com>
2021-09-25 18:07:25 -0500

    added basic form for making posts

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,10 @@
+export const NewPost = () => {
+  return (
+    <div>
+      <form action="submit">
+        <input type="text" />
+        <button>Make new post</button>
+      </form>
+    </div>
+  );
+};

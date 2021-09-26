2ce20be161a973a96ca18569eeb4e5e328134356
Eric Treadway <eric.s.treadway@gmail.com>
2021-09-26 18:23:49 -0500

    added react-router-dom to serve different content

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import { PostProvider } from "./context/PostsContext";
 import { Wrapper } from "./style/Wrapper";
 import { NewPost } from "./components/NewPost";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <PostProvider>
-      <Wrapper>
-        <h1>Hello from my blog!</h1>
-        <NewPost />
-        <Posts />
-      </Wrapper>
-    </PostProvider>
+    <Router>
+      <PostProvider>
+        <Wrapper>
+          <h1>Hello from my blog!</h1>
+
+          <Switch>
+            <Route path="/newpost">
+              <NewPost />
+            </Route>
+            <Route path="/">
+              <Posts />
+            </Route>
+          </Switch>
+        </Wrapper>
+      </PostProvider>
+    </Router>
   );
 }
 
